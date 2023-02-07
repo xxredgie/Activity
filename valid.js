4c2f7a8471d277eff3f29b3ab5ccab4da6dd8bdd
Redgie Sumawang <lunaticf156@gmail.com>
2023-02-07 09:10:59 +0800

    jscript changes

--- valid.js
+++ valid.js
@@ -2,7 +2,7 @@ function myfunction(){
     var x = document.getElementById("pass");
 
     if(x.type === "password"){
-        x.type = "email";
+        x.type = "text";
     }
     else{
         x.type = "password"
